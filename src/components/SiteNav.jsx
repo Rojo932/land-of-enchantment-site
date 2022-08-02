75ec155e7dbc156eea9fb179e2cf797ccd634a88
JonathanR <rojo932@gmail.com>
2022-08-02 14:28:41 -0700

    planned site routes

---
+++ src/components/SiteNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SiteNav() {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>
+          will be site nav
+        </h1>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default SiteNav
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>
+          Home page
+        </h1>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Home
